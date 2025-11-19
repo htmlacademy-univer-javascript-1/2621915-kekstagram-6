dbab746086b57e4a7da1903e217ea61d1a9afb10
Утемов Александр <utemovaleks@gmail.com>
2025-11-19 22:00:52 +0500

    Большая картинка

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
 import { generatePhotos } from './photos.js';
 import { renderThumbnails } from './thumbnails.js';
+import { openBigPicture } from './big-picture.js';
 
+const picturesContainer = document.querySelector('.pictures');
 const photos = generatePhotos();
 
 renderThumbnails(photos);
+
+picturesContainer.addEventListener('click', (evt) => {
+  const thumbnail = evt.target.closest('.picture');
+  if (!thumbnail) {return;}
+
+  const id = Number(thumbnail.dataset.photoId);
+  const photo = photos.find((p) => p.id === id);
+
+  if (photo) {
+    openBigPicture(photo);
+  }
+});
