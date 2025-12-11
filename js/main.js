802c74b4e2ad7961001b9c5f25d2b71f3f18ae46
Утемов Александр <utemovaleks@gmail.com>
2025-12-11 05:33:32 +0500

    переписал слайдер

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@ import { generatePhotos } from './photos.js';
 import { renderThumbnails } from './thumbnails.js';
 import { openBigPicture } from './big-picture.js';
 import { initForm } from './form.js';
-import { addFilter } from './effects.js';
+import { initImageEditor } from './effects.js';
 
 const picturesContainer = document.querySelector('.pictures');
 const photos = generatePhotos();
 
 renderThumbnails(photos);
+initImageEditor();
 initForm();
-addFilter();
 
 picturesContainer.addEventListener('click', (evt) => {
   const thumbnail = evt.target.closest('.picture');
